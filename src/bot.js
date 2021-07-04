6156719812e4756be225675c96c8ab2d6cbc522f
PYlamp <73482519+PYlamp@users.noreply.github.com>
2021-07-04 08:55:05 -0500

    added kick feature
    still some testing is needed but
    cmd = $kick (user_id)
    should kick use

--- src/bot.js
+++ src/bot.js
@@ -12,15 +12,26 @@ bot.on('ready', () => {
 bot.on('message', (message) => {
   if (message.author.bot) return;
   if (message.content.startsWith(PREFIX)) {
-    const [CMD_NAME, ...args] = message.content
+  const [CMD_NAME, ...args] = message.content
     .trim()
     .substring(PREFIX.length)
     .split(/\s+/);
-
-  console.log(CMD_NAME);
-  console.log(args);
+  if (CMD_NAME === 'kick') {
+      if (!message.member.hasPermission('KICK_MEMBERS'))
+        return message.reply('You dont have permissions to use that command');
+      if (args.length === 0) 
+        return message.reply('Please provide an ID');
+      const member = message.guild.members.cache.get(args[0]);
+      if (member) {
+        member
+          .kick()
+          .then((member) =>  message.channel.send(`${member} was kicked!`))
+          .catch((err) => message.channel.send('I do not have permission :('));
+      } else {
+        message.channel.send('That member was not found');
+      }
+    }
   }
-
 });
 // console.log(`[${message.author.tag}]: ${message.content}`);
 //   if (message.content === 'hello') {
